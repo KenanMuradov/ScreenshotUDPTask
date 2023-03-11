4856f0a5fe7e38aaccb8afd947dddca29c75c973
Mura_mw07 <Mura_mw07@STHQ012B-04>
2023-03-11 12:56:57 +0400

    Add project files.

---
+++ ClientApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp;
+
+public partial class MainWindow : Window
+{
+    private Socket client;
+    private EndPoint remoteEP;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        client = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp);
+
+
+        var ip = IPAddress.Parse("127.0.0.1");
+        var port = 45678;
+        remoteEP = new IPEndPoint(ip, port);
+
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var buffer = new byte[ushort.MaxValue - 29];
+        client.SendTo(buffer, remoteEP);
+
+        while (true)
+        {
+            var len = client.ReceiveFrom(buffer, SocketFlags.None, ref remoteEP);
+
+
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using System.Net.Sockets;
+using System.Net;
+
+
+
+
+var listener = new Socket(AddressFamily.InterNetwork,
+    SocketType.Dgram,
+    ProtocolType.Udp);
+
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 45678;
+
+var buffer = new byte[1024];
+var ep = new IPEndPoint(ip, port);
+listener.Bind(ep);
+
+
+EndPoint remoteEp = new IPEndPoint(IPAddress.Any, 0);
+
+
+while (true)
+{
+    var result = await listener.ReceiveFromAsync(buffer, SocketFlags.None, remoteEp);
+
+    Console.WriteLine("Hello");
+
+    var img = TakeScreenShot();
+
+    var imgBuffer = ImageToByte(img);
+
+    var chunk = imgBuffer.Chunk(ushort.MaxValue - 29);
+
+    var newBuffer = chunk.ToArray();
+
+    for (int i = 0; i < newBuffer.Length; i++)
+        listener.SendTo(newBuffer[i], result.RemoteEndPoint);
+}
+
+
+
+
+byte[] ImageToByte(Image img)
+{
+    using (var stream = new MemoryStream())
+    {
+        img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+        return stream.ToArray();
+    }
+}
+
+
+Image TakeScreenShot()
+{
+    Bitmap memoryImage;
+    memoryImage = new Bitmap(720, 480);
+
+    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+    memoryGraphics.CopyFromScreen(0, 0, 0, 0, memoryImage.Size);
+
+    return (Image)memoryImage;
+}
